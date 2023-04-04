1b01748ec5bbcfb14508927cde7539663ff04404
Никола Тесла <nionismine@yahoo.com.tw>
2023-04-05 00:04:07 +0800

    失敗的一個版本，待會要局部全部刪掉

--- JobCrwaler_104/CrawlingManager.cs
+++ JobCrwaler_104/CrawlingManager.cs
@@ -1,44 +1,49 @@
 ﻿using HtmlAgilityPack;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace JobCrwaler_104
 {
     public class CrawlingManager
     {
         private readonly BlackList _blackList;
+        private readonly HtmlDocument _document;
         private const string _sourcePath = "C:\\Users\\User\\Desktop\\my repo\\JobCrwaler_104\\JobCrwaler_104\\SourceHtml";
         private const string _outputPath = "C:\\Users\\User\\Desktop\\my repo\\JobCrwaler_104\\JobCrwaler_104\\Output.html";
 
         public CrawlingManager()
         {
             _blackList = new BlackList();
+            _document = new HtmlDocument();
         }
 
-        public async Task<int> ProcessAsync()
+        public async Task<(int, int)> ProcessAsync()
         {
-            var document = new HtmlDocument();
-            var filtered = new List<HtmlNode>();
+            //await PreTrimSourceHtml();
+
+
             var sourceHtml = await File.ReadAllTextAsync(_sourcePath);
+            _document.LoadHtml(sourceHtml);
 
-            document.LoadHtml(sourceHtml);
+            var xPathArtical = "//*[@id=\"js-job-content\"]/article";
+            var articles = _document.DocumentNode.SelectNodes(xPathArtical) ??
+                           throw new NullReferenceException("來源資料找不到artical節點");
 
-            var xPath = "//*[@id=\"js-job-content\"]/article";
-            var articles = document.DocumentNode.SelectNodes(xPath);
-
-            foreach (var article in articles)
-            {
-                if (!IsBlacklist(article))
-                {
-                    filtered.Add(article);
-                }
-            }
+            int originCount = articles.Count;
+            var filtered = articles.Where(article => !IsBlacklist(article))
+                                   .Where(article => IsSpecifiedDate(article, "3/31"))
+                                   .ToList();
 
             var outputHtml = GetHtmlString(filtered);
             await File.WriteAllTextAsync(_outputPath, outputHtml, Encoding.UTF8);
 
-            return filtered.Count;
+            return (originCount, filtered.Count);
         }
 
+        [Obsolete("尚未施工完成，暫無法使用")]
         private JobInfo DesrializeHtmlNodes(HtmlNode node)
         {
             return new JobInfo
@@ -53,20 +58,35 @@ namespace JobCrwaler_104
         private bool IsBlacklist(HtmlNode node)
         {
             var jobTitle = node.GetAttributeValue("data-job-name", null).ToUpper();
-            var companyName = node.GetAttributeValue("data-cust-name", null);
+            var company = node.GetAttributeValue("data-cust-name", null);
+            var industry = node.GetAttributeValue("data-indcat-desc", null);
 
-            if (_blackList.CompanyNames.Contains(companyName))
+            if (string.IsNullOrEmpty(jobTitle))
+            {
+                var wrongText = node.OuterHtml;
+                var fixedText = wrongText.Replace("data-job-name=\"\"", "data-job-name=\"");
+
+                _document.LoadHtml(fixedText);
+                var newNode = _document.DocumentNode;
+
+            }
+
+
+            if (_blackList.IndustryName.Contains(industry))
                 return true;
 
-            if (_blackList.CompanyNames_SeemsGoodButCurrentlyNo.Contains(companyName))
+            if (_blackList.CompanyNames.Contains(company))
+                return true;
+
+            if (_blackList.CompanyNames_SeemsGoodButCurrentlyNo.Contains(company))
                 return true; 
             
-            if (_blackList.CompanyNames_HadSubmitted.Contains(companyName))
+            if (_blackList.CompanyNames_HadSubmitted.Contains(company))
                 return true;
 
             foreach (var keyword in _blackList.CompanyKeywords)
             {
-                if (companyName.Contains(keyword))
+                if (company.Contains(keyword))
                     return true;
             }
 
@@ -79,15 +99,114 @@ namespace JobCrwaler_104
             return false;
         }
 
-        private string GetHtmlString(List<HtmlNode> nodes)
+        private bool IsSpecifiedDate(HtmlNode node, string date)
+        {
+            var xPathDateNode = ".//span[@class='b-tit__date']";
+            var text = node.SelectSingleNode(xPathDateNode).InnerText.Trim();
+
+            if (string.IsNullOrEmpty(text))
+                return true;
+
+            return text.Equals(date);
+        }
+
+        private string GetHtmlString(IList<HtmlNode> nodes)
         {
             var sb = new StringBuilder();
             for (int i = 0; i < nodes.Count; i++)
             {
                 sb.Append(nodes[i].OuterHtml);
             }
-
             return sb.ToString();
+        }
+
+        private async Task PreTrimSourceHtml()
+        {
+            using var fs = new FileStream(_sourcePath, FileMode.Open, FileAccess.ReadWrite);
+            using var reader = new StreamReader(fs);
+            using var writer = new StreamWriter(fs);
+            
+           
+
+            string? line;
+            var position = 0L;
+            var sb = new StringBuilder();
+            var context = "data-job-name=\"\"";
+
+
+            var AAA = fs.Position;
+            var BBB = fs.Position;
+            var RRRR1 = 0L;
+            var RRRR2 = 0L;
+            var RRRR3 = 0L;
+            var RRRR4 = 0L;
+
+            var totalLineCount1 = 0;
+            var totalLineCount2 = 0;
+            var totalLineCount3 = 0;
+            var totalLineCount4 = 0;
+
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                int index = line.IndexOf(context);
+
+                RRRR1 = Encoding.UTF8.GetByteCount(line);
+                RRRR2 = Encoding.ASCII.GetByteCount(line);
+                RRRR3 = Encoding.UTF32.GetByteCount(line);
+                RRRR4 = Encoding.Unicode.GetByteCount(line);
+
+                totalLineCount1 += (int)RRRR1;
+                totalLineCount2 += (int)RRRR2;
+                totalLineCount3 += (int)RRRR3;
+                totalLineCount4 += (int)RRRR4;
+
+
+                BBB = fs.Position;
+
+
+                // 覆蓋的行數錯誤，沒蓋在該蓋的地方
+                // 還是差一點點
+
+                // 後來我想想，好像可以捨棄掉這麼複雜的方法
+                // 直接用replace的方法執行
+
+
+                line.Replace(context, sb.ToString());
+
+                if (index != -1)
+                {
+                    sb.Append(line);
+                    sb.Remove(index + 15, 1);
+                    sb.Insert(index + 15, ' ');
+                    line = sb.ToString();
+
+                    var count = Encoding.UTF8.GetByteCount(line);
+                    fs.Seek(position - count, SeekOrigin.Begin);
+
+
+                    await fs.WriteAsync(Encoding.UTF8.GetBytes(line), 0, count);
+                    await fs.FlushAsync();
+                    
+                    //await writer.WriteLineAsync(line);
+                    
+                    //await writer.FlushAsync();
+
+                    //fs.Seek(position + count, SeekOrigin.Begin);
+
+
+                    sb.Clear();
+                }
+
+                position = fs.Position;
+
+                //11760900
+                //+15
+            }
+        }
+
+        private async Task PreTrimSourceHtmlTemp()
+        {
+
         }
     }
 }
--- JobCrwaler_104/Program.cs
+++ JobCrwaler_104/Program.cs
@@ -3,7 +3,51 @@
 var manager = new CrawlingManager();
 var quantity = await manager.ProcessAsync();
 
-
-
-
 Console.WriteLine($"共{quantity}筆篩選後資料");
+
+
+//using System.Text;
+
+//var pathSource = @"c:\tests\source.txt";
+//var pathNew = @"c:\tests\newfile.txt";
+
+
+//using (var source = new FileStream(pathSource, FileMode.Open, FileAccess.ReadWrite))
+//{
+//    var bytes = new byte[source.Length];
+//    int numBytesToRead = (int)source.Length;
+//    int numBytesRead = 0;
+
+//    while (numBytesToRead > 0)
+//    {
+//        // Read may return anything from 0 to numBytesToRead.
+//        int n = await source.ReadAsync(bytes, numBytesRead, numBytesToRead);
+
+//        var str = Encoding.UTF8.GetString(bytes);
+
+//        await Console.Out.WriteAsync(str);
+
+//        var replacement = "999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999";
+//        int n1 = await source.ReadAsync(bytes, numBytesRead, numBytesToRead);
+
+//        for (int i = 0; i < bytes.Length; i++)
+//        {
+//            await Console.Out.WriteAsync("");
+//        }
+
+//        // Break when the end of the file is reached.
+//        if (n == 0)
+//            break;
+
+//        numBytesRead += n;
+//        numBytesToRead -= n;
+//    }
+
+//    numBytesToRead = bytes.Length;
+
+//    // Write the byte array to the other FileStream.
+//    using (var fsNew = new FileStream(pathNew, FileMode.Create, FileAccess.Write))
+//    {
+//        fsNew.Write(bytes, 0, numBytesToRead);
+//    }
+//}
